09336fc3586ff9fe514ce647ad72a989a05ac2a0
djemo75 <djemsuleyman98@abv.bg>
2017-11-02 17:50:43 +0200

    homework

---
+++ GeometryCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option (triangle, square or radians)");
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "triangle":
+                    Console.WriteLine("Enter side:");
+                    double side = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter height:");
+                    double height = double.Parse(Console.ReadLine());
+                    double result = side * height / 2;
+                    Console.WriteLine("The area of triangle is {0}",result);
+                break;
+                case "square":
+                    Console.WriteLine("Enter side:");
+                    double side2 = double.Parse(Console.ReadLine());
+                    double result2 = side2 * side2;
+                    Console.WriteLine("The area of triangle is {0}", result2);
+                break;
+                case "radians":
+                    Console.WriteLine("Enter the degrees  you want to transform in radians:");
+                    double radians = double.Parse(Console.ReadLine());
+                    double result3 = radians * Math.PI / 180;
+                    Console.WriteLine("The area of triangle is {0}", result3);
+                break;
+
+                default:
+                    Console.WriteLine("Wrong option! Try again!!!");
+                    break;
+            }
+        }
+    }
+}
---
+++ GetMax/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            double first = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            double second = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number:");
+            double third = double.Parse(Console.ReadLine());
+
+            double result = Math.Max(first,Math.Max(second,third));
+            Console.WriteLine("The biggest number is {0}",result);
+        }
+    }
+}
